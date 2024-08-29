775476f2474514b9d10b870c42cdd724730d78bd
Cgristophe Irakoze <christophe.irakoze@aetradegroup.com>
2024-08-29 11:18:44 +0200

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-import': {},
+    // ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
+}
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-600">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
