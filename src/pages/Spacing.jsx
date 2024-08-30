cf06798ee1c5884e51ecda356dc407e6864de414
Cgristophe Irakoze <christophe.irakoze@aetradegroup.com>
2024-08-30 11:39:08 +0200

    add layout

---
+++ src/pages/Spacing.jsx
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+const Spacing = () => {
+  return (
+    <>
+      <Navigation />
+      <section>
+        <h1>SPACING</h1>
+      </section>
+    </>
+  );
+};
+
+export default Spacing;
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Customization from "../pages/customization";
+import Layout from "../pages/Layout";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/customization" element={<Customization />} />
+      <Route path="/layout" element={<Layout />} />
     </Routes>
   );
 };
